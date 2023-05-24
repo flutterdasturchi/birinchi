9fe7468bbcc99af4cca74a14764473d436f8e942
azizaka <ziz.uz3616524@gmail.com>
2023-05-24 10:13:16 +0500

    fff

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -106,7 +106,8 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'salom hammaga',
+              style: TextStyle(fontSize: 100),
             ),
             Text(
               '$_counter',
